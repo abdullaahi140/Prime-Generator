c28fc3210cd7a325662129c6fb98b8c87622e3ea
Abdullaahi Farah <abdullaahi140@gmail.com>
2017-09-10 21:21:19 +0100

    Label is showing last prime only

--- prime number gui.py
+++ prime number gui.py
@@ -19,7 +19,7 @@ class Window(QtGui.QMainWindow):
 
         label2 = QtGui.QLabel("",widget)
         label2.setStyleSheet("font: 20pt Proxima Nova")
-        self.label_updater(label2)
+        QtCore.QTimer.singleShot(1000, lambda: self.label_updater(label2))
 
         v_layout = QtGui.QVBoxLayout(widget)
         v_layout.addWidget(label,0,QtCore.Qt.AlignCenter)
@@ -29,25 +29,22 @@ class Window(QtGui.QMainWindow):
         self.setCentralWidget(widget)
         self.show()
 
-    def label_updater(label,self):
-        #p = [[prime for prime in Prime.obtain_range(100)] if prime is not None]
+    def label_updater(self,label2):
         p = []
         for prime in Prime.obtain_range(100):
             if prime is not None:
-                p.append(str(prime)
+                label2.setText(str(prime))
 
-
-        print (p)
-        label.setText(p.pop([0]))
+        QtCore.QTimer.singleShot(1000, lambda: self.label_updater(label2))
 
 def run():
     app = QtGui.QApplication(sys.argv)
     GUI = Window()
     sys.exit(app.exec_())
 
-    timer = QtCore.QTimer()
-    timer.timeout.connect(self.label_updater)
-    timer.start(10000)
+    # timer = QtCore.QTimer()
+    # timer.timeout.connect(self.label_updater)
+    # timer.start(10000)
 
 run()
 
