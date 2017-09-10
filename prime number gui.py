cbd5c122edc50b77e84e083073c095c7efff0d1e
Abdullaahi Farah <abdullaahi140@gmail.com>
2017-09-10 16:41:48 +0100

    Still can't automatically update the label

--- prime number gui.py
+++ prime number gui.py
@@ -1,38 +1,37 @@
 from PyQt4 import QtGui, QtCore
 import sys
 import Prime
-import time
 
 class Window(QtGui.QMainWindow):
 
     def __init__(self):
         super(Window, self).__init__()
         self.setWindowTitle("Prime number GUI")
-        #self.setStyleSheet("background: white")
-        self.label_updater()
+        self.setStyleSheet("background: white")
+        self.home()
         
-    def home(self,prime):
+    def home(self):
 
-        self.widget = QtGui.QWidget()
+        widget = QtGui.QWidget()
         
-        label = QtGui.QLabel("Prime Numbers", self.widget)
+        label = QtGui.QLabel("Prime Numbers", widget)
         label.setStyleSheet("font: 30pt Proxima Nova")
 
-        label2 = QtGui.QLabel(str(prime),self.widget)
+        label2 = QtGui.QLabel("",widget)
         label2.setStyleSheet("font: 20pt Proxima Nova")
         
-        v_layout = QtGui.QVBoxLayout(self.widget)
+        v_layout = QtGui.QVBoxLayout(widget)
         v_layout.addWidget(label,0,QtCore.Qt.AlignCenter)
         v_layout.addWidget(label2,0,QtCore.Qt.AlignCenter)
-        self.widget.setLayout(v_layout)
+        widget.setLayout(v_layout)
 
-        self.setCentralWidget(self.widget)
+        self.setCentralWidget(widget)
         self.show()
 
     def label_updater(self):
         for prime in Prime.obtain_range(100):
             if prime is not None:
-                self.home(prime)
+                label2.setText(str(prime))
 
 def run():
     app = QtGui.QApplication(sys.argv)
@@ -41,13 +40,8 @@ def run():
 
     timer = QtCore.QTimer()
     timer.timeout.connect(self.label_updater)
-    timer.start(50000)
+    timer.start(20000)
 
 run()
-
-
-
-
-
 
 #Having issues with updating label2
