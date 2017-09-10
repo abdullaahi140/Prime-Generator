f8d3766ac6a1516b0b6e7de0459f96f991f9bf91
abdullaahi140 <abdullaahi140@gmail.com>
2017-09-10 16:06:11 +0100

    Label is not updating

--- prime number gui.py
+++ prime number gui.py
@@ -9,37 +9,40 @@ class Window(QtGui.QMainWindow):
         super(Window, self).__init__()
         self.setWindowTitle("Prime number GUI")
         #self.setStyleSheet("background: white")
-        self.home()
+        self.label_updater()
         
-    def home(prime,self):
+    def home(self,prime):
 
-        widget = QtGui.QWidget()
+        self.widget = QtGui.QWidget()
         
-        label = QtGui.QLabel("Hello", widget)
+        label = QtGui.QLabel("Prime Numbers", self.widget)
         label.setStyleSheet("font: 30pt Proxima Nova")
 
-        label2 = QtGui.QLabel(str(prime),widget)
+        label2 = QtGui.QLabel(str(prime),self.widget)
         label2.setStyleSheet("font: 20pt Proxima Nova")
-
-        v_layout = QtGui.QVBoxLayout(widget)
+        
+        v_layout = QtGui.QVBoxLayout(self.widget)
         v_layout.addWidget(label,0,QtCore.Qt.AlignCenter)
         v_layout.addWidget(label2,0,QtCore.Qt.AlignCenter)
-        widget.setLayout(v_layout)
+        self.widget.setLayout(v_layout)
 
-        self.setCentralWidget(widget)
+        self.setCentralWidget(self.widget)
         self.show()
 
-        time.sleep(1)
-
-    def prime_updater(self):
-        for i in Prime.obtain_range(100):
-            if i is not None:
-                self.home(i)
+    def label_updater(self):
+        for prime in Prime.obtain_range(100):
+            if prime is not None:
+                self.home(prime)
 
 def run():
     app = QtGui.QApplication(sys.argv)
     GUI = Window()
     sys.exit(app.exec_())
+
+    timer = QtCore.QTimer()
+    timer.timeout.connect(self.label_updater)
+    timer.start(50000)
+
 run()
 
 
@@ -47,4 +50,4 @@ run()
 
 
 
-Having issues with updating label2
+#Having issues with updating label2
