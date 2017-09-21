30f860f4245e6e386dd7b0a68eb2d929a4158bd0
Abdullaahi Farah <abdullaahi140@gmail.com>
2017-09-21 18:37:43 +0100

    Cleaning

--- prime number gui.py
+++ prime number gui.py
@@ -7,30 +7,30 @@ class Window(QtGui.QMainWindow):
     def __init__(self):
         super(Window, self).__init__()
         self.setWindowTitle("Prime number GUI")
-        #self.setStyleSheet("background: white")
-        self.home()
-        
-    def home(self):
+        self.setStyleSheet("background: white")
+        self.show_primes()
 
-        widget = QtGui.QWidget()
+    def show_primes(self):
+
+        widget2 = QtGui.QWidget()
         
-        label = QtGui.QLabel("Prime Numbers", widget)
+        label = QtGui.QLabel("Prime Numbers", widget2)
         label.setStyleSheet("font: 30pt Proxima Nova")
 
-        label2 = QtGui.QLabel("2",widget)
+        label2 = QtGui.QLabel("2",widget2)
         label2.setStyleSheet("font: 20pt Proxima Nova")
         QtCore.QTimer.singleShot(1000, lambda: self.label_updater(label2))
 
-        v_layout = QtGui.QVBoxLayout(widget)
-        v_layout.addWidget(label,0,QtCore.Qt.AlignCenter)
+        v_layout = QtGui.QVBoxLayout(widget2)
+        v_layout.addWidget(label,0,QtCore.Qt.AlignLeft)
         v_layout.addWidget(label2,0,QtCore.Qt.AlignCenter)
-        widget.setLayout(v_layout)
+        widget2.setLayout(v_layout)
 
-        self.setCentralWidget(widget)
+        self.setCentralWidget(widget2)
         self.show()
 
     def label_updater(self,label2):
-        testnum = int(label2.text())+1
+        testnum = int(label2.text()) + 1
         prime = Prime.obtain_prime(testnum)
         while prime is None:
             testnum += 1
@@ -42,9 +42,5 @@ def run():
     app = QtGui.QApplication(sys.argv)
     GUI = Window()
     sys.exit(app.exec_())
-
-    # timer = QtCore.QTimer()
-    # timer.timeout.connect(self.label_updater)
-    # timer.start(10000)
 
 run()
